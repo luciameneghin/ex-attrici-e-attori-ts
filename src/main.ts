c55f6855a7543d4f4e19d396af428e9ce3098e74
luciameneghin <lucia.meneghin00@gmail.com>
2025-05-11 15:39:12 +0200

    feat: milestone 2

--- src/main.ts
+++ src/main.ts
@@ -7,3 +7,11 @@ type Person = {
   biography: string;
   image: string;
 }
+
+// Milestone 2
+
+type Actress = Person & {
+  most_famous_movies: [string, string, string];
+  awards: string;
+  nationality: 'American' | 'British' | 'Australian' | 'Israeli-American' | 'South African' | 'French' | 'Indian' | 'Israeli' | 'Spanish' | 'South Korean' | 'Chinese';
+}
